6e5c0cf1d0ddf74f77ddaca903690feadd60a564
leschenkoartem <leschenko.artem@lll.kpi.ua>
2023-11-12 00:48:23 +0200

    Initial Commit

---
+++ EnergyTreck/EnergyTreckApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyTreckApp.swift
+//  EnergyTreck
+//
+//  Created by Artem Leschenko on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct EnergyTreckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
